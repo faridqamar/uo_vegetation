7b2024406918fc678d7e930a37aa483e1aaffc17
faridqamar <farid.qamar@gmail.com>
2019-09-15 16:54:22 +0000

    save mouse clicks to file

--- pixel_select.py
+++ pixel_select.py
@@ -3,18 +3,30 @@
 import numpy as np
 import cv2
 
-class LoadImage:
-	def loadImage(self):
-		self.img-=cv2.imread('./output/scene_RGB_00108.png')
-		cv2.imshow('Test', self.img)
-		
-		self.pressedkey = cv2.waitKey(0)
+coordList = dict(x=[], y=[])
 
-		#Wait for Esc key to exit
-		if self.pressedkey==27:
-			cv2.destroyAllWindows()
+def draw_circle(event, x, y, flags, param):
+	#global mouseX, mouseY
+	if event == cv2.EVENT_LBUTTONDOWN:
+		cv2.circle(img, (x, y), 1, (255, 0, 0), -1)
+		#mouseX, mouseY = x, y
+		coordList['x'].append(x)
+		coordList['y'].append(y)
+		print("(%d, %d)" %(x, y))
 
-	#Start of the main program here
-	if __name__ = "__main__":
-		LI = LoadImage()
-		LI..loadImage()
+img = cv2.imread('./output/scene_RGB_00108.png')
+cv2.namedWindow('./output/scene_RGB_00108.png')
+cv2.setMouseCallback('./output/scene_RGB_00108.png', draw_circle)
+
+while(1):
+	cv2.imshow('./output/scene_RGB_00108.png', img)
+	k = cv2.waitKey(20) & 0xFF
+	#print(mouseX, mouseY)
+	if k == 27:
+		print("Printing coordinate list to file")# %(len(coordList['x'])))
+		f = open("./coordinates.txt", "w")
+		f.write(str(coordList))
+		f.close()
+		break
+	#elif k == ord('a'):
+	#	print(mouseX, mouseY)
