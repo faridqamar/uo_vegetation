2b2a2a20011e5f600d12412d3c30820df4cca030
faridqamar <farid.qamar@gmail.com>
2019-09-15 17:42:16 +0000

    save mouse clicks to file

--- pixel_select.py
+++ pixel_select.py
@@ -2,16 +2,46 @@
 
 import numpy as np
 import cv2
+import json
 
-coordList = dict(x=[], y=[])
+val = 0
+classType = ['sky', 'vegetation', 'built']
+
+def getClassType(val, classType):
+	classType = ['sky', 'vegetation', 'built']
+	print("Which class of pixels are you selecting?")
+	print("Enter the number associate with the class you wish to select:")
+	print("1) %s" %(classType[0]))
+	print("2) %s" %(classType[1]))
+	print("3) %s" %(classType[2]))
+	class_type = int(input("class type = ")) - 1
+	return class_type
+
+while(1):
+	class_type = getClassType(val, classType)	
+	try:
+		val = int(class_type)
+		if(val in [0, 1, 2]):
+			print("%s class type selected" %(classType[val].upper()))
+			break
+		else:
+			print("")			
+			print("ERROR: The class type selected is invalid please try again")
+	except ValueError:
+		print("")
+		print("ERROR: The class type selected is invalid please try again")
+			 
+filename = "./" + classType[class_type] + "_coordinates.txt"
+xcoord = []
+ycoord = []
 
 def draw_circle(event, x, y, flags, param):
 	#global mouseX, mouseY
 	if event == cv2.EVENT_LBUTTONDOWN:
 		cv2.circle(img, (x, y), 1, (255, 0, 0), -1)
 		#mouseX, mouseY = x, y
-		coordList['x'].append(x)
-		coordList['y'].append(y)
+		xcoord.append(x)
+		ycoord.append(y)
 		print("(%d, %d)" %(x, y))
 
 img = cv2.imread('./output/scene_RGB_00108.png')
@@ -21,12 +51,17 @@ cv2.setMouseCallback('./output/scene_RGB_00108.png', draw_circle)
 while(1):
 	cv2.imshow('./output/scene_RGB_00108.png', img)
 	k = cv2.waitKey(20) & 0xFF
-	#print(mouseX, mouseY)
 	if k == 27:
-		print("Printing coordinate list to file")# %(len(coordList['x'])))
-		f = open("./coordinates.txt", "w")
-		f.write(str(coordList))
+		print("Printing coordinate list to file %s" %filename)
+		f = open(filename, "a+")
+		for i in range(len(xcoord)):
+			f.write("%d %d\n" %(xcoord[i], ycoord[i]))
 		f.close()
 		break
 	#elif k == ord('a'):
 	#	print(mouseX, mouseY)
+
+
+
+
+
