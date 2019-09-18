d7ba4af2316f4d30866af37575b1382f3890db76
faridqamar <farid.qamar@gmail.com>
2019-09-18 20:27:34 +0000

    write rind/cind to file

--- pixel_select_ginput.py
+++ pixel_select_ginput.py
@@ -3,9 +3,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-'''
+
 val = 0
 classType = ['sky', 'vegetation', 'built']
+
 
 def getClassType(val, classType):
 	classType = ['sky', 'vegetation', 'built']
@@ -16,6 +17,7 @@ def getClassType(val, classType):
 	print("3) %s" %(classType[2]))
 	class_type = int(input("class type = ")) - 1
 	return class_type
+
 
 while(1):
 	class_type = getClassType(val, classType)	
@@ -41,10 +43,7 @@ try:
 	print("---")
 except FileNotFoundError:
 	print("---")
-'''
 
-xcoord = []
-ycoord = []
 
 img = mpimg.imread('./output/scene_RGB_00108.png')
 
@@ -53,68 +52,23 @@ fig = plt.figure(figsize=(10, 5), dpi=80)
 ax = fig.add_axes([0.05, 0.05, 0.9, 0.9])
 ax.imshow(img, interpolation='none', aspect=0.5)
 
-def loc_to_rowcol(xloc, yloc):
-    cind = int(round(xloc))
-    rind = int(round(yloc))
-
-    return rind, cind
-
 
 def onclick(event):
     if event.dblclick:
-        circle = plt.Circle((event.xdata, event.ydata), 10, color='blue')
+        circle = plt.Circle((event.xdata, event.ydata), 2, color='blue')
         ax.add_patch(circle)
         fig.canvas.draw()
-        print(loc_to_rowcol(event.xdata, event.ydata))
+
+        cind = int(round(event.xdata))
+        rind = int(round(event.ydata))
+        print("(%d, %d)" % (rind, cind))
+
+        f = open(filename, "a+")
+        f.write("%d %d\n" % (rind, cind))
+        f.close()
+    
         
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
 plt.show()
-
-
-
-
-'''
-
-def draw_circle(event, x, y, flags, param):
-	if event == cv2.EVENT_LBUTTONDOWN:
-		cv2.circle(img, (x, y), 1, (255, 0, 0), -1)
-		xcoord.append(x)
-		ycoord.append(y)
-		print("(%d, %d)" %(x, y))
-
-# read image
-img = cv2.imread('./output/scene_RGB_00108.png')
-cv2.namedWindow('./output/scene_RGB_00108.png')
-cv2.setMouseCallback('./output/scene_RGB_00108.png', draw_circle)
-
-# open image and begin selecting pixels
-while(1):
-	cv2.imshow('./output/scene_RGB_00108.png', img)
-	k = cv2.waitKey(20) & 0xFF
-	# if 10 pixels are selected, print them to file
-	if len(xcoord) == 10:
-		print("10 coordinates selected")
-		print("Printing coordinate list to file %s" %filename)
-		f = open(filename, "a+")
-		for i in range(len(xcoord)):
-			f.write("%d %d\n" %(xcoord[i], ycoord[i]))
-		f.close()
-		print("%d coordinates successfully written to file %s" %(len(xcoord), filename))
-		xcoord = []
-		ycoord = []
-	# to exit click the Esc button, any selected pixels will be added to the file
-	if k == 27:
-		print("Printing coordinate list to file %s" %filename)
-		f = open(filename, "a+")
-		for i in range(len(xcoord)):
-			f.write("%d %d\n" %(xcoord[i], ycoord[i]))
-		f.close()
-		print("%d coordinates successfully written to file %s" %(len(xcoord), filename))
-		break
-	#elif k == ord('a'):
-	#	print(mouseX, mouseY)
-
-
-'''
 
 
